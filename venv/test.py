7987e959493ec483190031f52252092d568c23fd
UngeheurenUngeziefer <seweruskein2@gmail.com>
2020-06-18 19:47:49 +0300

    Removed folder from repository

--- venv/test.py
+++
@@ -1,8 +0,0 @@
-a = input('Why do you create me?')
-
-if a is not None:
-    b = input('Is that the reason to create something?')
-    if b is not None:
-        a = input('Why do you create me?')
-else:
-
